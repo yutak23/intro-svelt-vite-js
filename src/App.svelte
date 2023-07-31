7559db4dfab18b7d3ce01f615d779b292ac96cd1
yutak23 <yutapilafu@gmail.com>
2023-07-31 18:09:57 +0900

    feat: Part 1 / Reactivity / Statements

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,17 @@
 <script>
 	let count = 0;
-	$: doubled = count * 2;
 
-	const increment = () => {
+	$: if (count >= 10) {
+		alert('count is dangerously high!');
+		count = 0;
+	}
+
+	const handleClick = () => {
 		count += 1;
 	};
 </script>
 
-<button on:click={increment}>
+<button on:click={handleClick}>
 	Clicked {count}
 	{count === 1 ? 'time' : 'times'}
 </button>
-
-<p>{count} doubled is {doubled}</p>
