508507077351ab3fe05a3752172e03c6f89404cb
yutak23 <yutapilafu@gmail.com>
2023-07-31 16:58:49 +0900

    fix: eslint error

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+// eslint-disable-next-line import/no-unresolved
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
