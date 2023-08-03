4a3b0a91ef4cc381dff740f4f49c0390e59127ff
yutak23 <yutapilafu@gmail.com>
2023-08-03 17:53:22 +0900

    feat: Part 1 / Logic / Else blocks

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script>
 	let count = 0;
 
-	const increment = () => {
+	function increment() {
 		count += 1;
-	};
+	}
 </script>
 
 <button on:click={increment}>
@@ -11,6 +11,10 @@
 	{count === 1 ? 'time' : 'times'}
 </button>
 
+<!-- #：ifブロックの開始を意味する -->
+<!-- /：ifブロックの終了を意味する -->
 {#if count > 10}
 	<p>{count} is greater than 10</p>
+{:else}
+	<p>{count} is between 0 and 10</p>
 {/if}
