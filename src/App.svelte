84d9f7ebab711ab42f8817f5570cd760866cc07f
yutak23 <yutapilafu@gmail.com>
2023-08-28 15:42:41 +0900

    feat: Part 2 / Transitions / Adding parameters

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
 	// eslint-disable-next-line import/no-unresolved
-	import { fade } from 'svelte/transition';
+	import { fly } from 'svelte/transition';
 
 	let visible = true;
 </script>
@@ -11,5 +11,5 @@
 </label>
 
 {#if visible}
-	<p transition:fade>Fades in and out</p>
+	<p transition:fly={{ y: 200, duration: 2000 }}>Flies in and out</p>
 {/if}
