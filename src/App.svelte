68f0f0938c7cf5fc57f06545f48f243ab831c362
yutak23 <yutapilafu@gmail.com>
2023-08-28 12:15:33 +0900

    feat: Part 1 / Stores / Auto-subscriptions

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,23 @@
 <script>
+	// import { onDestroy } from 'svelte';
 	import { count } from './stores';
 	import Incrementer from './Incrementer.svelte';
 	import Decrementer from './Decrementer.svelte';
 	import Resetter from './Resetter.svelte';
 
-	let countValue;
+	// let countValue;
 
-	count.subscribe((value) => {
-		countValue = value;
-	});
+	// const unsubscribe = count.subscribe((value) => {
+	// 	countValue = value;
+	// });
+
+	// コンポーネントが何度もインスタンス化されたり破棄されたりしてもメモリーリークにならないようにサブスクリプションを破棄する
+	// が、onDestroyでunsubscribeするのも冗長なので$でstoreを参照すると自動的にunsubscribeされる
+	// onDestroy(unsubscribe);
 </script>
 
-<h1>The count is {countValue}</h1>
+<!-- <h1>The count is {countValue}</h1> -->
+<h1>The count is {$count}</h1>
 
 <Incrementer />
 <Decrementer />
