2e68e9a797a89e2620594dc8370954378d53ff6d
yutak23 <yutapilafu@gmail.com>
2023-08-28 15:41:23 +0900

    feat: Part 2 / Transitions / The transition directive

--- src/App.svelte
+++ src/App.svelte
@@ -1,63 +1,15 @@
 <script>
 	// eslint-disable-next-line import/no-unresolved
-	import { spring } from 'svelte/motion';
+	import { fade } from 'svelte/transition';
 
-	const coords = spring(
-		{ x: 50, y: 50 },
-		{
-			stiffness: 0.1,
-			damping: 0.25
-		}
-	);
-
-	const size = spring(10);
+	let visible = true;
 </script>
 
-<svg
-	role="presentation"
-	on:mousemove={(e) => {
-		coords.set({ x: e.clientX, y: e.clientY });
-	}}
-	on:mousedown={() => size.set(30)}
-	on:mouseup={() => size.set(10)}
->
-	<circle cx={$coords.x} cy={$coords.y} r={$size} />
-</svg>
+<label>
+	<input type="checkbox" bind:checked={visible} />
+	visible
+</label>
 
-<div class="controls">
-	<label>
-		<h3>stiffness ({coords.stiffness})</h3>
-		<input bind:value={coords.stiffness} type="range" min="0.01" max="1" step="0.01" />
-	</label>
-
-	<label>
-		<h3>damping ({coords.damping})</h3>
-		<input bind:value={coords.damping} type="range" min="0.01" max="1" step="0.01" />
-	</label>
-</div>
-
-<style>
-	svg {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		left: 0;
-		top: 0;
-	}
-
-	circle {
-		fill: #ff3e00;
-	}
-
-	.controls {
-		position: absolute;
-		top: 1em;
-		right: 1em;
-		width: 200px;
-		user-select: none;
-	}
-
-	.controls input {
-		width: 100%;
-	}
-</style>
+{#if visible}
+	<p transition:fade>Fades in and out</p>
+{/if}
