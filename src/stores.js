951009e134e0df72e99cbc26771ccd50652c8e72
yutak23 <yutapilafu@gmail.com>
2023-08-28 10:54:32 +0900

    feat: Part 1 / Stores / Writable stores

---
+++ src/stores.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-unresolved
+import { writable } from 'svelte/store';
+
+// eslint-disable-next-line import/prefer-default-export
+export const count = writable(0);
